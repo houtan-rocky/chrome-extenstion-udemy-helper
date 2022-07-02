e1d9e496e969db82c28bd923888c448ab9d7ffd3
Hootan Abasi Rocky <mast9rmind@gmail.com>
2022-07-03 02:33:42 +0430

    catch fetching error

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 // server connections
 async function getCourseDataFromServer(id) {
-    const res = await fetch(`https://www.udemy.com/api-2.0/courses/${id}/?fields[course]=created,title`, {
-        method: 'GET',
-    });
-    return await res.json();
+    try {
+        const res = await fetch(`https://www.udemy.com/api-2.0/courses/${id}/?fields[course]=created,title`, {
+            method: 'GET',
+        });
+        return await res.json();
+    } catch (e) {
+        console.warn("Error fetching creation date")
+    }
 }
 // !
+
+
+
 
 
 async function renderTitle () {
@@ -14,7 +21,7 @@ async function renderTitle () {
     const creationDateTime = new Date(courseData['created']);
     const creationDate = creationDateTime.toLocaleDateString()
     const courseTitle = document.querySelector('[data-purpose="lead-title"]');
-    courseTitle.innerHTML += "<br> creation date: " + creationDate;
+    courseTitle.innerHTML += "<br> Creation Date: " + creationDate;
 }
 
 
